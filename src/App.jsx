6f65805c40fe6affc1a25623972c5422240aa5ec
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-01-24 22:28:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+ 
+
+  return (
+    <>
+      <p class="text-green-400">HIJAU</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// Node Module
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// Css
+import './index.css'
+
+// Components
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
