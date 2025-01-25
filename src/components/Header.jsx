e84e8f845beb24bfb34ec985a9abfc4cc028c7a0
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-01-25 08:25:28 +0700

    [ADD] Membaut navar dan cssnya, kemudian penyesuaian pada header agar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
+// Node Module
+import {useState} from "react";
+
+// COMPONENTS
+import Navbar from './Navbar';
+
 const Header = () => {
+    const [navOpen, setNavOpen] =useState(false);
+
     return (
         <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
             <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]'>
@@ -15,14 +23,14 @@ const Header = () => {
                     </a>
                 </h1>
                 <div className='relative md:justify-self-center'>
-                    <button className='mennu-btn md:hidden' onClick={() => {}}>
+                    <button className='menu-btn md:hidden' onClick={() => setNavOpen((prev) => !prev)}>
                         <span className='material-symbols-rounded'>
-                            menu
+                            {navOpen ? 'close' : 'menu'}
                         </span>
                     </button>
-                    <span>Navbar</span>
+                    <Navbar navOpen={navOpen}/>
                 </div>
-                <a href="#contact" className=''>Contact Me</a>
+                <a href="#contact" className='btn  btn-secondary max-md:hidden md:justify-self-end'>Contact Me</a>
 
             </div>
         </header>
