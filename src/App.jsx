a241769ef42f7578dd96745b03d83e517fb74a76
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-01-26 10:26:08 +0700

    [ADD] Membuat component about untuk halaman portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Components
 import Header from './components/Header'
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
     <Header/>
    <main>
     <Hero/>
+    <About/>
    </main>
     </>
   )
